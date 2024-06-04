9eb96b85586bae3f2aee2f1bf56ae5c95e686746
Axel Ortiz <1723110322@utectulancingo.edu.mx>
2024-06-04 02:35:44 +0000

    UPDATE README

---
+++ aplicacion/sql_t.py
@@ -0,0 +1,26 @@
+#Importa las 03 librerias necesarias
+import requests as rs
+import json as js
+import base64
+
+#Codigo para transformar una imagen a base64
+with open('sql.jpg', 'rb') as f:
+    convert = base64.b64encode(f.read())
+
+img = convert.decode('utf-8')
+
+
+
+#Codigo para que la IA responda
+url = 'http://127.0.0.1:11434/api/generate'
+myobj = {
+  "model": "codellama",
+  "prompt":"Can you make the script for SQL for this picture?",
+  "images": [img],
+  "stream": False
+}
+
+x = rs.post(url, json = myobj)
+x = js.loads(x.text)
+
+print(x['response'])
